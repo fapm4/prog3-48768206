6928186111e840d47e9b805a6bc9e841e591d486
alu <alu@VDI-Ubuntu-EPS-2019>
2020-12-01 11:05:18 +0100

    GIF 2D hecho

--- prog3-battleship/src/model/io/VisualiserGIF.java
+++ prog3-battleship/src/model/io/VisualiserGIF.java
@@ -8,6 +8,7 @@ import model.Game;
 import model.exceptions.io.BattleshipIOException;
 import model.io.gif.AnimatedGIF;
 import model.io.gif.FrameGIF;
+import model.ship.Board2D;
 
 public class VisualiserGIF implements IVisualiser{
 
@@ -39,10 +40,7 @@ public class VisualiserGIF implements IVisualiser{
 			if(c == Board.HIT_SYMBOL) {
 				frame.printSquare(col, fila, Color.RED);
 			}
-			
-			agif.addFrame(frame);
-			
-
+					
 		}
 		catch(BattleshipIOException e) {
 			throw new RuntimeException();
@@ -51,27 +49,61 @@ public class VisualiserGIF implements IVisualiser{
 	}	
 	
 	
-	private void printBoard(Board b, FrameGIF frame) {
-		String cadB1 = b.show(false);
+	private void printBoard(Board b, FrameGIF frame, boolean bType) {
+		String cad = b.show(false);
+		int tamCadena = cad.length();
+		int tamBoard = b.getSize();
 		
-		for(int i = 0;i < cadB1.length(); i++) {
-			for(int j = 0;j < b.getSize(); j++) {
-				addFrameAux(i, j, cadB1.charAt(i), frame);
+		if(!bType) {
+			for(int i = 0;i < tamCadena && i != tamBoard; i++) {
+				for(int j = 0;j < tamBoard; j++) {
+					addFrameAux(j, i, cad.charAt(i), frame);
+				}
+			}
+		}
+		else {
+			for(int i = 0;i < tamCadena &&  i != tamBoard; i++) {
+				for(int j = tamBoard + 1;j <= tamBoard * 2;j++) {
+					addFrameAux(j, i, cad.charAt(i), frame);
+				}
 			}
 		}
 	}
 	
-
+	
+	private void añadeFranjas(FrameGIF frame) {
+		Board b = game.getBoard1();
+		int tam = b.getSize();
+		
+		
+		for(int i = 0;i < tam;i++) {
+			try {
+				frame.printSquare(i, tam, Color.DARK_GRAY);
+			} catch (BattleshipIOException e) {
+				throw new RuntimeException();
+			}
+		}
+		
+	}
 	
 	
 	public void show() {
-		FrameGIF frame = new FrameGIF(5, 5*2+1);
 		Board b1 = game.getBoard1();
 		Board b2 = game.getBoard2();
+		FrameGIF frame = new FrameGIF(b1.getSize(), b1.getSize()*2+1);
 		
-		printBoard(b1, frame);
-		printBoard(b2, frame);
-		
+		if(b1 instanceof Board2D && b2 instanceof Board2D) {
+			printBoard(b1, frame, false);
+			añadeFranjas(frame);
+			printBoard(b2, frame, true);
+			
+			try {
+				agif.addFrame(frame);
+			}
+			catch(BattleshipIOException e) {
+				throw new RuntimeException();
+			}
+		}
 		
 	}
 	
