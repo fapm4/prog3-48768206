5c39045df7f3bf9f29a424eed48aa67a51beb9c4
alu <alu@VDI-Ubuntu-EPS-2019>
2020-12-03 18:16:43 +0100

    Listo

--- prog3-battleship/src/model/io/VisualiserGIF.java
+++ prog3-battleship/src/model/io/VisualiserGIF.java
@@ -27,50 +27,9 @@ public class VisualiserGIF implements IVisualiser{
 	}
 	
 	
-	private void addFrameAux(int fila, int col, char c, FrameGIF frame) {
-		try {
-			if(c == Board.NOTSEEN_SYMBOL) {
-				frame.printSquare(col, fila, Color.LIGHT_GRAY);
-			}
-			
-			if(c == Board.WATER_SYMBOL) {
-				frame.printSquare(col, fila, Color.BLUE);
-			}
-			
-			if(c == Board.HIT_SYMBOL) {
-				frame.printSquare(col, fila, Color.RED);
-			}
-		}
-		catch(BattleshipIOException e) {
-			throw new RuntimeException();
-		}
-	}	
-	
-	
-	private void printBoard(Board b, FrameGIF frame, boolean bType) {
-		String cad = b.show(false);
-		int tamBoard = b.show(false).length();
-		
-		if(!bType) {
-			for(int i = 0;i < tamBoard; i++) {
-				for(int j = 0;j < tamBoard; j++) {
-					addFrameAux(i, j, cad.charAt(j), frame);
-				}
-			}
-		}
-		else {
-			for(int i = 0;i < tamBoard; i++) {
-				for(int j = tamBoard + 1;j <= tamBoard * 2;j++) {
-					addFrameAux(j, i, cad.charAt(i), frame);
-				}
-			}
-		}
-	}
-	
-	
-	private void añadeFranjas(FrameGIF frame) {
+	private void addFranjas(FrameGIF frame) {
 		Board b = game.getBoard1();
-		int tam = b.show(false).length();
+		int tam = b.getSize();
 		
 		
 		for(int i = 0;i < tam;i++) {
@@ -84,24 +43,78 @@ public class VisualiserGIF implements IVisualiser{
 	}
 	
 	
+	private void auxAddFrame(int i, int j, char c, FrameGIF frame) {
+		
+		try {
+			if(c == Board.NOTSEEN_SYMBOL) {
+				frame.printSquare(j, i, Color.LIGHT_GRAY);
+			}
+			
+			if(c == Board.WATER_SYMBOL) {
+				frame.printSquare(j, i, Color.BLUE);
+			}
+			
+			if(c == Board.HIT_SYMBOL) {
+				frame.printSquare(j, i, Color.RED);
+			}
+			
+			if(c == Board.Board_SEPARATOR) {
+				frame.printSquare(j, i, Color.ORANGE);
+			}
+		}
+		catch (BattleshipIOException e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	private void printBoard(Board b, FrameGIF frame, boolean typeBoard) {
+		int tamBoard = b.getSize();
+		String cadBoard = b.show(false);
+		
+		
+		// Board 1
+		if(typeBoard) {
+			for(int i = 0; i < tamBoard;i++) {
+				for(int j = 0;j < tamBoard;j++) {
+					//System.out.println(i + " " + j);
+					auxAddFrame(j, i, cadBoard.charAt(j), frame);
+				}
+			}
+		}
+		
+		// Board 2
+		else {
+			for(int i = 0;i < tamBoard;i++) {
+				for(int j = tamBoard + 1;j <= tamBoard * 2;j++) {
+					auxAddFrame(j, i, cadBoard.charAt(i), frame);
+				}
+			}
+		}
+	}
+	
+	
 	public void show() {
 		Board b1 = game.getBoard1();
 		Board b2 = game.getBoard2();
-		int tamFrame = b1.show(false).length();
+		int tamFrame = b1.getSize();
 		FrameGIF frame = new FrameGIF(tamFrame, tamFrame*2+1);
 		
 		if(b1 instanceof Board2D) {
-			printBoard(b1, frame, false);
-			añadeFranjas(frame);
-			printBoard(b2, frame, true);
-			
-			try {
-				agif.addFrame(frame);
-			}
-			catch(BattleshipIOException e) {
-				throw new RuntimeException();
-			}
+			printBoard(b1, frame, true);
+			addFranjas(frame);
+			printBoard(b2, frame, false);
 		}
+		
+		else {
+			
+		}
+		
+		try {
+			agif.addFrame(frame);
+		}catch(BattleshipIOException e) {
+			throw new RuntimeException();
+		}
+		
 	}
 	
 
